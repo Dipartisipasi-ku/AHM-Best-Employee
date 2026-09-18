5293a5d124d68dde65ec4347c9921b31856b2f02
Dipartisipasi-ku <dipartisipasi.ku@gmail.com>
2026-09-18 21:33:41 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+// env.js
+window.env = {
+  // Ganti dengan URL dan Anon Key dari Dashboard Supabase Anda
+  SUPABASE_URL: "https://mpbemgtefasaqcoomico.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wYmVtZ3RlZmFzYXFjb29taWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MDc5NDAsImV4cCI6MjEwNTI4Mzk0MH0.sZrFr0f6C2gam-2oh1dqYn2YbLjWOlM8C017Wz4IqfU"
+};
